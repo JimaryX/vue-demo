78b2c94b7029a0859d44ae369fb59936cb9fba50
jimaryxia <erkang.xia@capgemini.com>
2019-09-19 10:03:12 +0800

    登录界面

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import "@/assets/styles/index.scss";
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
